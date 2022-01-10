565f8d6b98c43d7c8fbdf2cd61a69e38b955df3f
Max Mustermann <max.mustermann@windev-iad.org>
2022-01-10 14:57:21 +0100

    öouihpi

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string name = "Hans";
+string name2 = name;
+Console.WriteLine("Du heißt {0}",name2);
+Console.WriteLine("Du heißt " + name);
+Console.WriteLine($"Du heißt {name}");
+
