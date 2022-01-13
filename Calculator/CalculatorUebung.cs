4e1dd1e766292aff756797e70725891aa30799a3
Max Mustermann <max.mustermann@windev-iad.org>
2022-01-13 15:15:45 +0100

    zukz

--- Calculator/CalculatorUebung.cs
+++ Calculator/CalculatorUebung.cs
@@ -8,6 +8,8 @@ internal class CalculatorUebung
     //Methodensichtbarkeit: innerhalb derselben Assembly für andere Klassen und deren Methoden
     public static void Main(string[] args) // string[] args hier nicht nötig, bleibt leer und wird nicht genutzt
     {
+
+
         Console.WriteLine("Willkommen beim Summatorrr numero zwo!");
         Console.WriteLine("bitte zwei oder mehr Summanden eingeben (durch Leerzeichen trennen): ");
         Console.WriteLine("[Ohne Leerzeichen wird nur die Quersumme gebildet :D]");
@@ -15,7 +17,7 @@ internal class CalculatorUebung
         string[] z = str.Split();
         if (z.Length < 2)
         {
-            Console.WriteLine("Die Quersumme von {0} ist {1}",,);
+            Console.WriteLine("Die Quersumme von {0} ist {1}", str, Quer(str));
         }
         else
         {
@@ -33,15 +35,17 @@ internal class CalculatorUebung
 
     }
 
-    public int Quer(string eingabe)
+    public static int Quer(string eingabe)
     {
         int qsum = 0;
-        char[] ch = Convert.ToChar(eingabe);
         foreach (char ch in eingabe)
         {
-            int z = Convert.ToInt32(ch);
+
+            int z = Convert.ToInt32(Convert.ToString(ch));
             qsum += z;
         }
+        return qsum;
+
     }
 }
 
